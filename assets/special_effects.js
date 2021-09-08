c0b4d7b483dc93ba25ef09efb920c71808f16e7d
Ana Carolina K <anacko@gmail.com>
2021-09-08 15:18:42 +0000

    add different bar sizes

--- assets/special_effects.js
+++ assets/special_effects.js
@@ -1,24 +1,34 @@
 /* Filename: special_effects.js */
-$( document ).ready(function() {
-  $( "a" ).click(function( event ) {
-    alert( "Thanks for visiting!" );
-  });
+const transformUserData = function(userData) {
+  userData = userData.replace("[", "");
+  userData = userData.replace("]", "");
+  userData = userData.split(', ');
+  for (let i = 0; i<userData.length; i++) {
+    userData[i] = Number(userData[i])
+  }
+  return userData;
+}
+let userData = "";
 
-  let userData = "";
+$( document ).ready(function() {
+
   $("button").click(function(event)  {
-    userData = document.getElementById("userData").value;  
-    alert("You clicked the button to make a chart. " + userData);
-    userData = userData.replace("[", "");
-    userData = userData.replace("]", "");
-    userData = userData.split(', ');
-    for (let i = 0; i<userData.length; i++) {
-      userData[i] = Number(userData[i])
+    userData = document.getElementById("userData").value;
+    const barSizes = transformUserData(userData);
+    alert("You clicked the button to make a chart. " + barSizes);
+    for (const size of barSizes) {
+     $("#theChart").append('<p class="bar" style="width:'+size*100+'px;">New bar here.</p>')
     }
   });
 
   $ ( ".bar" ).click(function( event ) {
     alert("You clicked ");
   });
+
+  $( "a" ).click(function( event ) {
+    alert( "Thanks for visiting!" );
+  });
+
 
 });
 
